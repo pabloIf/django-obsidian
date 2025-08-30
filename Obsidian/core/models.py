97f15a587a388526e6a5527bc3e3933f566eff8a
Pavlo Saltikov <aknelsaltikov@email.com>
2025-08-30 14:11:37 +0300

    modify urls, views, write templates for catalog and products

--- Obsidian/core/models.py
+++ Obsidian/core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -12,6 +13,10 @@ class Category(models.Model):
     
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse("core:product_list_by_category", kwargs={'category_slug': self.slug})
+    
     
 
 class Product(models.Model):
@@ -31,3 +36,6 @@ class Product(models.Model):
     
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse("core:product_detail", kwargs={'id': self.id, 'slug': self.slug})
